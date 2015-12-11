fe68d78da0973a0a6d5def8913af573d8410e8d0
Michael McCune <msm@redhat.com>
2015-12-11 14:22:35 -0500

    update caravan_master for amqp changes
    * make mongo and rest urls arguments
    * reduce to a single socket
    * removing extra fields from data packets to align with new packing

--- whirlwind_caravan/caravan_master.py
+++ whirlwind_caravan/caravan_master.py
@@ -1,4 +1,5 @@
 import datetime
+import sys
 import uuid
 
 import pymongo
@@ -7,56 +8,58 @@ from pyspark.streaming import StreamingContext
 import requests
 
 
-def signal_rest_server(rawdata):
+def signal_rest_server(rawdata, rest_url):
     data = {'id': rawdata['_id'],
             'count': rawdata['count'],
-            'errors': rawdata['errors'],
-            'service': rawdata['service'],
             }
-    requests.post('http://localhost:9050/count-packets', json=data)
+    requests.post(rest_url, json=data)
 
 
-def store_packets(data):
+def store_packets(data, mongo_url):
     if data['count'] != 0:
-        db = pymongo.MongoClient('10.0.1.107').sparkhara.count_packets
+        db = pymongo.MongoClient(mongo_url).sparkhara.count_packets
         db.insert_one(data)
 
 
 def normalize_log_lines(log_lines, service_name=None):
-    contains_error = False
-    for l in log_lines:
-        try:
-            sl = l.split('::')
-            if 'ERROR' in sl[2]:
-                contains_error = True
-        except IndexError:
-            pass
     data = {'_id': None if len(log_lines) == 0 else uuid.uuid4().hex,
             'count': len(log_lines),
-            'service': service_name,
-            'errors': contains_error,
             'logs': log_lines,
             }
     return data
 
 
-def process_generic(rdd, service_name):
+def process_generic(rdd, mongo_url, rest_url):
     log_lines = rdd.collect()
-    data = normalize_log_lines(log_lines, service_name)
+    print(log_lines)
+    data = normalize_log_lines(log_lines)
     data['processed-at'] = datetime.datetime.now().strftime(
         '%Y-%m-%d %H:%M:%S.%f')[:-3]
-    store_packets(data)
-    signal_rest_server(data)
+    store_packets(data, mongo_url)
+    signal_rest_server(data, rest_url)
 
 
-if __name__ == '__main__':
+def print_usage():
+    print('caravan_master requires 2 arguments, mongo url and rest url')
+    print('example:')
+    print('spark-submit caravan_master.py mongodb://127.0.0.1 http://127.0.0.1/endpoint')
+
+
+def main():
+    if len(sys.argv) != 3:
+        print_usage()
+        return
+    mongo_url = sys.argv[1]
+    rest_url = sys.argv[2]
     sc = SparkContext(appName='SparkharaLogCounter')
     ssc = StreamingContext(sc, 1)
 
-    sahara_lines = ssc.socketTextStream('0.0.0.0', 9901)
-    sahara_lines.foreachRDD(lambda rdd: process_generic(rdd, 'sahara'))
+    lines = ssc.socketTextStream('0.0.0.0', 9901)
+    lines.foreachRDD(lambda rdd: process_generic(rdd, mongo_url, rest_url))
 
-    neutron_lines = ssc.socketTextStream('0.0.0.0', 9902)
-    neutron_lines.foreachRDD(lambda rdd: process_generic(rdd, 'neutron'))
     ssc.start()
     ssc.awaitTermination()
+
+
+if __name__ == '__main__':
+    main()
