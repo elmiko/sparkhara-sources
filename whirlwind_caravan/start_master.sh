57a8341a804b350bf5848781e5fd3461c27f802c
Matthew Farrellee <matt@redhat.com>
2015-12-12 09:32:57 -0500

    add master start script to setup name resolution

---
+++ whirlwind_caravan/start_master.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+. /common.sh
+
+MASTER_SERVICE_NAME=${1//-/_}
+echo "$(eval echo \$${MASTER_SERVICE_NAME^^}_SERVICE_HOST) spark-master" >> /tmp/hosts
+
+MONGODB_SERVICE_NAME=${2//-/_}
+echo "$(eval echo \$${MONGODB_SERVICE_NAME^^}_SERVICE_HOST) mongodb" >> /tmp/hosts
+
+# because the hostname only resolves locally
+export SPARK_LOCAL_HOSTNAME=$(hostname -i)
+
+spark-submit /caravan_master.py --port 1984 \
+                                --master spark://spark-master:7077 \
+                                --mongo mongo://mongodb \
+                                --rest http:XYZ
