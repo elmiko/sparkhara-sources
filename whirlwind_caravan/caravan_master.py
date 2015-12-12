891a2276f85f5e63e24a6797b9a972c139875487
Matthew Farrellee <matt@redhat.com>
2015-12-12 14:37:55 -0500

    reduce logging

--- whirlwind_caravan/caravan_master.py
+++ whirlwind_caravan/caravan_master.py
@@ -33,7 +33,7 @@ def normalize_log_lines(log_lines, service_name=None):
 
 def process_generic(rdd, mongo_url, rest_url):
     log_lines = rdd.collect()
-    print(log_lines)
+    print(len(log_lines), "processed")
     data = normalize_log_lines(log_lines)
     data['processed-at'] = datetime.datetime.now().strftime(
         '%Y-%m-%d %H:%M:%S.%f')[:-3]
