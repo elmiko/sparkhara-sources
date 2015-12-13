550b1ab3fe001afb522b6cbc15a351484df6ef6a
Matthew Farrellee <matt@redhat.com>
2015-12-13 08:51:59 -0500

    simplify connecting to mongo service
    there's no need to map it in the hosts database like the spark-master

--- whirlwind_caravan/start_master.sh
+++ whirlwind_caravan/start_master.sh
@@ -5,8 +5,7 @@
 MASTER_SERVICE_NAME=${1//-/_}
 echo "$(eval echo \$${MASTER_SERVICE_NAME^^}_SERVICE_HOST) spark-master" >> /tmp/hosts
 
-MONGODB_SERVICE_NAME=${2//-/_}
-echo "$(eval echo \$${MONGODB_SERVICE_NAME^^}_SERVICE_HOST) mongodb" >> /tmp/hosts
+MONGODB_SERVICE_NAME=$2
 
 # because the hostname only resolves locally
 export SPARK_LOCAL_HOSTNAME=$(hostname -i)
@@ -15,5 +14,5 @@ MONGO_USER=$(cat /etc/mongo-secret/username)
 MONGO_PASS=$(cat /etc/mongo-secret/password)
 
 spark-submit /caravan_master.py --master spark://spark-master:7077 \
-                                --mongo mongodb://${MONGO_USER}:${MONGO_PASS}@mongodb/sparkhara \
+                                --mongo mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGODB_SERVICE_NAME}/sparkhara \
                                 --rest http:XYZ
