36956f99b4778aa9f68d93909bde21f3b636ad2a
Matthew Farrellee <matt@redhat.com>
2015-12-13 08:45:37 -0500

    use secrets instead of hardcoding user/pass

--- whirlwind_caravan/start_master.sh
+++ whirlwind_caravan/start_master.sh
@@ -11,6 +11,9 @@ echo "$(eval echo \$${MONGODB_SERVICE_NAME^^}_SERVICE_HOST) mongodb" >> /tmp/hos
 # because the hostname only resolves locally
 export SPARK_LOCAL_HOSTNAME=$(hostname -i)
 
+MONGO_USER=$(cat /etc/mongo-secret/username)
+MONGO_PASS=$(cat /etc/mongo-secret/password)
+
 spark-submit /caravan_master.py --master spark://spark-master:7077 \
-                                --mongo mongodb://sparkhara:openstack@mongodb/sparkhara \
+                                --mongo mongodb://${MONGO_USER}:${MONGO_PASS}@mongodb/sparkhara \
                                 --rest http:XYZ
